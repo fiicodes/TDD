f864e9d67c0bf5e5a662e2384e3dd61d4a16be90
fithaashraf17 <fithaashraf172@gmail.com>
2022-12-29 13:33:00 +0530

    Modified tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -12,10 +15,10 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
         self.assertIn('To-Do', header_text)
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
         self.assertEqual(
         inputbox.get_attribute('placeholder'),
         'Enter a to-do item'
@@ -27,11 +30,13 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_element(By.TAG_NAME,'tr')
         self.assertTrue(
-        any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+ any(row.text == '1: Buy peacock feathers' for row in rows),
+ f"New to-do item did not appear in table. Contents were:\n{table.text}"
+)
+
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
