417c8c6cd1738d1968fb6ad3e3a056dc501d1dcc
fithaashraf17 <fithaashraf172@gmail.com>
2023-01-30 19:37:32 +0530

    refactored functional test

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -11,17 +11,8 @@ class HomePageTest(TestCase):
         response=self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-        self.assertTemplateUsed(response, 'home.html')
 
-    def test_uses_home_template(self):
-        response=self.client.get('/')
-        self.assertTemplateUsed(response,'home.html')
+
 
     def test_can_save_a_POST_request(self):
         response=self.client.post('/',data={'item_text':'A new list item'})
--- text.py
+++ text.py
@@ -1,16 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-from selenium.webdriver.common.keys import Keys
-import time
-import unittest
-class NewVisitorTest(unittest.TestCase):
-    def setUp(self):
-          self.browser = webdriver.Firefox()
-    def tearDown(self):
-        self.browser.quit()
-
-    def check_for_row_in_list_table(self, row_text):
+def check_for_row_in_list_table(self, row_text):
         table =self.browser.find_element(By.ID,'id_list_table')
         rows = table.find_elements(By.TAG_NAME,'tr')
         self.assertIn(row_text, [row.text for row in rows])
@@ -47,7 +35,7 @@ class NewVisitorTest(unittest.TestCase):
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
-        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
@@ -64,7 +52,3 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
-
-if __name__=='__main__':
-    unittest.main(warnings='ignore')
